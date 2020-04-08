72f3b48996c46ce148ea8d13d78d06618fb6efc9
Robin-Castellani <robin.castellani@studenti.unitn.it>
2020-04-08 07:31:00 +0200

    Send messages to telegram bot

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ import csv
 import os
 import hashlib
 import requests
+import time
+import argparse
+import telegram
 
 # TODO: parallelizza con ThreadPoolExecutor
 #   guarda https://realpython.com/python-concurrency/#threading-version
-# TODO: invia i siti cambiati via Telegram ad un bot
 # TODO: commenta
 
 
@@ -44,16 +46,37 @@ def write_csv_data(csv_file_path, data):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('token', help='Telegram bot token')
+    parser.add_argument('chatid', help='ID of the chat opened with your')
+    args = parser.parse_args()
+    token = args.token
+    chat_id = args.chatid
+
+    bot = telegram.Bot(token=token)
+
+    wait_time = 60 * 60 * 12
+
     file_path = os.path.join(os.curdir, 'websites.csv')
 
-    websites_hashes = get_csv_data(file_path)
+    while True:
+        websites_hashes = get_csv_data(file_path)
 
-    for website, previous_hash in websites_hashes.items():
-        byte_response = open_website(website)
-        new_hash = get_sha256(byte_response)
+        changed_list = list()
 
-        if new_hash != previous_hash:
-            print(f'{website} è cambiato!')
-            websites_hashes[website] = new_hash
+        for website, previous_hash in websites_hashes.items():
+            print(f'Cheching {website}')
+            byte_response = open_website(website)
+            new_hash = get_sha256(byte_response)
 
-    write_csv_data(file_path, websites_hashes)
+            if new_hash != previous_hash:
+                changed_list.append(f'{website} è cambiato!')
+                websites_hashes[website] = new_hash
+
+        if len(changed_list) != 0:
+            bot.send_message(chat_id=chat_id, text='\n\n'.join(changed_list))
+
+        write_csv_data(file_path, websites_hashes)
+
+        print('Waiting {wait_time}')
+        time.sleep(wait_time)
